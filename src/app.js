eec028abed9ade8fd6e9efa9e81c1929cdf2bf25
vitoriacurty <vitoria.curty@hotmail.com>
2023-06-23 15:25:58 -0300

    add POST na rota /sign-up

--- src/app.js
+++ src/app.js
@@ -12,8 +12,20 @@ const tweets = [
   }
 ]
 
+const user = [
+  {
+    username: "bobesponja",
+    avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
+  }]
+
 app.get("/tweets", (req, res) => {
   res.send(tweets)
+})
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body
+  user.push({ username, avatar })
+  res.send("Ok")
 })
 
 const PORT = 5000
